f1bde618d0ea854e84cd95e160b9c96725dd6ecc
TheClutchPowers <dervieuxguillaume@gmail.com>
2025-02-22 11:04:14 -0500

    sccript debut

--- Assets/script/mouvement.cs
+++ Assets/script/mouvement.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class mouvement : MonoBehaviour
 {
+    private Rigidbody _rb;
+    private Animator _animator;
+    [SerializeField] private float _vitessePromenade;
+    private Vector3 directionInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+         _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnBouger (InputValue directionBase) {
+        Vector2 directionAvecVitesse = directionBase.Get<Vector2>() * _vitessePromenade;
+        directionInput = new Vector3(directionAvecVitesse.x, 0f, directionAvecVitesse.y);
     }
 }
